fa61a356cd75bf9ad5f510618a9bbf1b7762d055
Joshua Matos <josh@joshuamatos.com>
2022-05-05 08:23:32 -0500

    first commit

---
+++ src/main/java/com/joshuamatos/todopractice1/TodoPractice1Application.java
@@ -0,0 +1,13 @@
+package com.joshuamatos.todopractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoPractice1Application.class, args);
+	}
+
+}
